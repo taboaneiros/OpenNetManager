2e07ec79f9d617d57e0098ebb9a0d2c64fe0bcb9
taboaneiros <166648996+taboaneiros@users.noreply.github.com>
2026-08-17 15:33:46 -0300

    Remove static file collection from entrypoint
    Removed static file collection during entrypoint execution.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,5 @@ sleep 5
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
 echo "Starting application..."
 exec "$@"
