79109b1a8d1274fffd26740b0e3fcb1067f6302f
taboaneiros <166648996+taboaneiros@users.noreply.github.com>
2026-08-17 15:18:00 -0300

    Enhance entrypoint script to collect static files
    Added commands to collect static files before starting the application.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-# entrypoint.sh
+set -e
 
 echo "Waiting for database to be ready..."
 sleep 5
 
 echo "Running migrations..."
 python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 echo "Starting application..."
 exec "$@"
